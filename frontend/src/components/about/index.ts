302744d60d2d3734a0920868b93f2f04e52ee3b4
Jeff Barragan <leshz@me.com>
2025-07-24 01:58:24 +0000

    Remove AboutProfile from About component

--- frontend/src/components/about/index.ts
+++ frontend/src/components/about/index.ts
@@ -1,5 +1,4 @@
 export { AboutHero } from './AboutHero';
-export { AboutProfile } from './AboutProfile';
 export { AboutBio } from './AboutBio';
 export { AboutCredentials } from './AboutCredentials';
 export { AboutBooks } from './AboutBooks';
