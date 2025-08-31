f40443c2512bf32b34f16d26d0fd934a08111eca
Jeff Barragan <leshz@me.com>
2025-08-31 18:29:07 +0000

    chore: update Strapi packages to version 5.23.1 in backend and add global type definitions in frontend

--- backend/config/plugins.ts
+++ backend/config/plugins.ts
@@ -3,4 +3,7 @@ export default () => ({
     enabled: true,
     // resolve: './src/plugins/mercadopago-strapi'
   },
+  'populate-all': {
+    enabled: true
+  }
 });
--- backend/types/generated/components.d.ts
+++ backend/types/generated/components.d.ts
@@ -1,5 +1,124 @@
 import type { Schema, Struct } from '@strapi/strapi';
 
+export interface GeneralBarraDeNavegacion extends Struct.ComponentSchema {
+  collectionName: 'components_general_barra_de_navegacions';
+  info: {
+    displayName: 'Barra de navegacion';
+    icon: 'brush';
+  };
+  attributes: {
+    Icono: Schema.Attribute.Media<'images' | 'files'> &
+      Schema.Attribute.Required;
+    Link: Schema.Attribute.Component<'shared.accion', false>;
+  };
+}
+
+export interface GeneralColumna extends Struct.ComponentSchema {
+  collectionName: 'components_general_columnas';
+  info: {
+    displayName: 'Columna';
+    icon: 'apps';
+  };
+  attributes: {
+    Contenido: Schema.Attribute.RichText & Schema.Attribute.Required;
+    Titulo: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface GeneralDerechosDeAutor extends Struct.ComponentSchema {
+  collectionName: 'components_general_derechos_de_autors';
+  info: {
+    displayName: 'Derechos de autor';
+    icon: 'user';
+  };
+  attributes: {
+    autor: Schema.Attribute.RichText;
+  };
+}
+
+export interface GeneralPieDePagina extends Struct.ComponentSchema {
+  collectionName: 'components_general_pie_de_paginas';
+  info: {
+    displayName: 'Columnas';
+  };
+  attributes: {
+    autor: Schema.Attribute.Component<'general.derechos-de-autor', false>;
+    Columnas: Schema.Attribute.Component<'general.columna', true>;
+  };
+}
+
+export interface MercadopagoCustomer extends Struct.ComponentSchema {
+  collectionName: 'components_mercadopago_customer';
+  info: {
+    displayName: 'Customer';
+    icon: 'emotionHappy';
+  };
+  attributes: {
+    dni: Schema.Attribute.BigInteger & Schema.Attribute.Required;
+    email: Schema.Attribute.Email & Schema.Attribute.Required;
+    last_name: Schema.Attribute.String & Schema.Attribute.Required;
+    name: Schema.Attribute.String & Schema.Attribute.Required;
+    phone: Schema.Attribute.BigInteger & Schema.Attribute.Required;
+  };
+}
+
+export interface MercadopagoFulfillment extends Struct.ComponentSchema {
+  collectionName: 'components_mercadopago_fulfillment';
+  info: {
+    description: '';
+    displayName: 'fulfillment';
+    icon: 'exit';
+  };
+  attributes: {
+    address: Schema.Attribute.String & Schema.Attribute.Required;
+    city: Schema.Attribute.String & Schema.Attribute.Required;
+    department: Schema.Attribute.String & Schema.Attribute.Required;
+    message: Schema.Attribute.Text;
+    postal_code: Schema.Attribute.BigInteger;
+  };
+}
+
+export interface MercadopagoInformation extends Struct.ComponentSchema {
+  collectionName: 'components_mercadopago_product_information';
+  info: {
+    description: '';
+    displayName: 'information';
+    icon: 'bell';
+  };
+  attributes: {
+    information: Schema.Attribute.Blocks & Schema.Attribute.Required;
+    title: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
+export interface MercadopagoPromotion extends Struct.ComponentSchema {
+  collectionName: 'components_mercadopago_promotion';
+  info: {
+    description: '';
+    displayName: 'Promotion';
+    icon: 'walk';
+  };
+  attributes: {
+    best_seller: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    discount_tag: Schema.Attribute.String;
+    new: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    price_with_discount: Schema.Attribute.Integer;
+    recommended: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+    with_discount: Schema.Attribute.Boolean & Schema.Attribute.DefaultTo<false>;
+  };
+}
+
+export interface SharedAccion extends Struct.ComponentSchema {
+  collectionName: 'components_shared_accions';
+  info: {
+    displayName: 'accion';
+  };
+  attributes: {
+    Link: Schema.Attribute.String & Schema.Attribute.Required;
+    Texto: Schema.Attribute.String & Schema.Attribute.Required;
+  };
+}
+
 export interface SharedOpenGraph extends Struct.ComponentSchema {
   collectionName: 'components_shared_open_graphs';
   info: {
@@ -54,6 +173,15 @@ export interface SharedSeo extends Struct.ComponentSchema {
 declare module '@strapi/strapi' {
   export module Public {
     export interface ComponentSchemas {
+      'general.barra-de-navegacion': GeneralBarraDeNavegacion;
+      'general.columna': GeneralColumna;
+      'general.derechos-de-autor': GeneralDerechosDeAutor;
+      'general.pie-de-pagina': GeneralPieDePagina;
+      'mercadopago.customer': MercadopagoCustomer;
+      'mercadopago.fulfillment': MercadopagoFulfillment;
+      'mercadopago.information': MercadopagoInformation;
+      'mercadopago.promotion': MercadopagoPromotion;
+      'shared.accion': SharedAccion;
       'shared.open-graph': SharedOpenGraph;
       'shared.seo': SharedSeo;
     }
