a8b1e15bce07ce2802b8bc0c862c4c02ed5106bc
Jeff Barragan <leshz@me.com>
2025-12-07 13:26:23 +0000

    feat: Implementar controlador 'findOne' para la entidad 'blog' con sanitización de consultas y respuestas

--- backend/src/api/blog/controllers/blog.ts
+++ backend/src/api/blog/controllers/blog.ts
@@ -4,4 +4,24 @@
 
 import { factories } from '@strapi/strapi'
 
-export default factories.createCoreController('api::blog.blog');
+export default factories.createCoreController('api::blog.blog', ({ strapi }) => ({
+  async findOne(ctx) {
+    const { id: slug } = ctx.params;
+    const { query } = ctx;
+
+    const sanitizedQuery = await this.sanitizeQuery(ctx);
+
+    const entity = await strapi.documents('api::blog.blog').findFirst({
+      filters: { slug },
+      ...sanitizedQuery,
+    });
+
+    if (!entity) {
+      return ctx.notFound('Blog not Found');
+    }
+
+    const sanitizedEntity = await this.sanitizeOutput(entity, ctx);
+
+    return this.transformResponse(sanitizedEntity);
+  },
+}));
