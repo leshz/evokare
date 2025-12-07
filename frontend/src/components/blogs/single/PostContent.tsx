f5502f9c976e85b5c148990b355e078cd0670919
Jeff Barragan <leshz@me.com>
2025-12-07 13:19:23 +0000

    refactor: Eliminar componentes de imagen y título en PostContent para simplificar la estructura

--- frontend/src/components/blogs/single/PostContent.tsx
+++ frontend/src/components/blogs/single/PostContent.tsx
@@ -8,14 +8,6 @@ interface PostContentProps {
 export const PostContent = ({ title, image, content }: PostContentProps) => {
   return (
     <div className="md:col-span-2">
-      <Image
-        src={image}
-        width={400}
-        height={80}
-        alt={title}
-        className="mb-8 h-72 w-full rounded-2xl object-cover"
-      />
-      <h2 className="mb-4 text-3xl font-bold text-gray-900">{title}</h2>
       {content && (
         <div
           className="prose max-w-none text-gray-700"
